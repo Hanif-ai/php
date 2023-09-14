7fb99e4f9657e7b2c5a24fc52255b978c726027e
Md Hanif Mia <67138388+Hanif-ai@users.noreply.github.com>
2023-09-14 19:26:05 +0600

    Add files via upload

---
+++ personal_info.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Hanif Mia";
+$age = 23;
+$country = "Bangladesh";
+$aim = "Web Developer";
+echo "Hello I am $name and I am $age years old. I live in $country at Kalmakanda.
+I want to be $aim that's why I'm learning PHP programming language";
